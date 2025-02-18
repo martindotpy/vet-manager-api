24a0d051c4b99620bb27e503789f3b607a44574b
Martín Alexander Ramos Yampufé <114754842+martindotpy@users.noreply.github.com>
2025-02-18 15:56:37 -0500

    Revert "test(race): Test DELETE /race/{id} implemented"
    This reverts commit f190b1cf06ed3815692bdc2ec389edf5b0ac5fff.

--- src/test/java/com/vluepixel/vetmanager/api/race/core/integration/DeleteRaceIntegrationTest.java
+++ src/test/java/com/vluepixel/vetmanager/api/race/core/integration/DeleteRaceIntegrationTest.java
@@ -1,15 +1,5 @@
 package com.vluepixel.vetmanager.api.race.core.integration;
 
-import static com.vluepixel.vetmanager.api.auth.core.data.AuthDataProvider.BEARER_ADMIN_JWT;
-import static com.vluepixel.vetmanager.api.auth.core.data.AuthDataProvider.BEARER_USER_JWT;
-import static com.vluepixel.vetmanager.api.race.core.data.UpdateRaceDataProvider.INVALID_ID_NOT_FOUND_UPDATE_RACE_REQUEST;
-import static com.vluepixel.vetmanager.api.race.core.data.UpdateRaceDataProvider.VALID_UPDATE_RACE_REQUEST;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.function.Function;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -19,140 +9,20 @@ import com.vluepixel.vetmanager.api.base.BaseIntegrationTest;
  * Integration tests for the delete race functionality.
  */
 public class DeleteRaceIntegrationTest extends BaseIntegrationTest {
-    private static final String MESSAGE_OK = "Raza eliminada exitosamente";
-    private static final Function<String, String> MESSAGE_NOT_FOUND = parameter -> String
-            .format("Raza con id %s no encontrado(a)", parameter);
     // -----------------------------------------------------------------------------------------------------------------
     // Without authentication:
     // -----------------------------------------------------------------------------------------------------------------
-
-    @Test
-    void noUser_DeleteRaceWithValidParams_Forbidden() throws Exception {
-        mockMvc.perform(delete("/race/{id}", VALID_UPDATE_RACE_REQUEST.getId()))
-                .andExpect(status().isForbidden())
-                .andExpect(jsonPath("$.message").value(MESSAGE_FORBIDDEN));
-    }
-
-    @Test
-    void noUser_DeleteRaceWithInvalidParams_ID_NotFound_Forbidden() throws Exception {
-        mockMvc.perform(delete("/race/{id}", INVALID_ID_NOT_FOUND_UPDATE_RACE_REQUEST.getId()))
-                .andExpect(status().isForbidden())
-                .andExpect(jsonPath("$.message").value(MESSAGE_FORBIDDEN));
-    }
-
-    @Test
-    void noUser_DeleteRaceWithInvalidParams_ID_Negative_Forbidden() throws Exception {
-        mockMvc.perform(delete("/race/{id}", -10))
-                .andExpect(status().isForbidden())
-                .andExpect(jsonPath("$.message").value(MESSAGE_FORBIDDEN));
-    }
-
-    @Test
-    void noUser_DeleteRaceWithInvalidParams_ID_NotNumber_Forbidden() throws Exception {
-        mockMvc.perform(delete("/race/{id}", "abcde"))
-                .andExpect(status().isForbidden())
-                .andExpect(jsonPath("$.message").value(MESSAGE_FORBIDDEN));
-    }
 
     // -----------------------------------------------------------------------------------------------------------------
     // With authentication:
     // -----------------------------------------------------------------------------------------------------------------
 
     // Role: USER
-    @Test
-    @DirtiesContext
-    void user_DeleteRaceWithValidParams_Ok() throws Exception {
-        mockMvc.perform(delete("/race/{id}", VALID_UPDATE_RACE_REQUEST.getId())
-                .header("Authorization", BEARER_USER_JWT))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message").value(MESSAGE_OK));
-    }
-
-    @Test
-    void user_DeleteRaceWithInvalidParams_ID_NotFound_NotFound() throws Exception {
-        mockMvc.perform(delete("/race/{id}", INVALID_ID_NOT_FOUND_UPDATE_RACE_REQUEST.getId())
-                .header("Authorization", BEARER_USER_JWT))
-                .andExpect(status().isNotFound())
-                .andExpect(
-                        jsonPath("$.message").value(
-                                MESSAGE_NOT_FOUND
-                                        .apply(Integer.toString(INVALID_ID_NOT_FOUND_UPDATE_RACE_REQUEST.getId()))));
-    }
-
-    @Test
-    void user_DeleteRaceWithInvalidParams_ID_Negative_UnprocessableEntity() throws Exception {
-        mockMvc.perform(delete("/race/{id}", -10)
-                .header("Authorization", BEARER_USER_JWT))
-                .andExpect(status().isUnprocessableEntity())
-                .andExpectAll(
-                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
-                        jsonPath("$.details").isArray(),
-                        jsonPath("$.details.length()").value(1),
-                        jsonPath("$.details[0].field").value("query.id"),
-                        jsonPath("$.details[0].messages.length()").value(1),
-                        jsonPath("$.details[0].messages").value("El id debe ser mayor a 0"));
-    }
-
-    @Test
-    void user_DeleteRaceWithInvalidParams_ID_NotNumber_UnprocessableEntity() throws Exception {
-        mockMvc.perform(delete("/race/{id}", "abcde")
-                .header("Authorization", BEARER_USER_JWT))
-                .andExpect(status().isUnprocessableEntity())
-                .andExpectAll(
-                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
-                        jsonPath("$.details").isArray(),
-                        jsonPath("$.details.length()").value(1),
-                        jsonPath("$.details[0].field").value("path.id"),
-                        jsonPath("$.details[0].messages.length()").value(1),
-                        jsonPath("$.details[0].messages").value("Illegal argument: For input string: \"abcde\""));
-    }
 
     // Role: ADMIN
     @Test
     @DirtiesContext
     void admin_DeleteRaceWithValidParams_Ok() throws Exception {
-        mockMvc.perform(delete("/race/{id}", VALID_UPDATE_RACE_REQUEST.getId())
-                .header("Authorization", BEARER_ADMIN_JWT))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message").value(MESSAGE_OK));
-    }
 
-    @Test
-    void admin_DeleteRaceWithInvalidParams_ID_NotFound_NotFound() throws Exception {
-        mockMvc.perform(delete("/race/{id}", INVALID_ID_NOT_FOUND_UPDATE_RACE_REQUEST.getId())
-                .header("Authorization", BEARER_ADMIN_JWT))
-                .andExpect(status().isNotFound())
-                .andExpect(
-                        jsonPath("$.message").value(
-                                MESSAGE_NOT_FOUND
-                                        .apply(Integer.toString(INVALID_ID_NOT_FOUND_UPDATE_RACE_REQUEST.getId()))));
-    }
-
-    @Test
-    void admin_DeleteRaceWithInvalidParams_ID_Negative_UnprocessableEntity() throws Exception {
-        mockMvc.perform(delete("/race/{id}", -10)
-                .header("Authorization", BEARER_ADMIN_JWT))
-                .andExpect(status().isUnprocessableEntity())
-                .andExpectAll(
-                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
-                        jsonPath("$.details").isArray(),
-                        jsonPath("$.details.length()").value(1),
-                        jsonPath("$.details[0].field").value("query.id"),
-                        jsonPath("$.details[0].messages.length()").value(1),
-                        jsonPath("$.details[0].messages").value("El id debe ser mayor a 0"));
-    }
-
-    @Test
-    void admin_DeleteRaceWithInvalidParams_ID_NotNumber_UnprocessableEntity() throws Exception {
-        mockMvc.perform(delete("/race/{id}", "abcde")
-                .header("Authorization", BEARER_ADMIN_JWT))
-                .andExpect(status().isUnprocessableEntity())
-                .andExpectAll(
-                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
-                        jsonPath("$.details").isArray(),
-                        jsonPath("$.details.length()").value(1),
-                        jsonPath("$.details[0].field").value("path.id"),
-                        jsonPath("$.details[0].messages.length()").value(1),
-                        jsonPath("$.details[0].messages").value("Illegal argument: For input string: \"abcde\""));
     }
 }
