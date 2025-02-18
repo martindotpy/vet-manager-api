1725f4fec2ccd87f84f860f8a20500306d5919a5
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-18 15:49:13 -0500

    feat: Add exception handlers

--- src/main/java/com/vluepixel/vetmanager/api/shared/adapter/out/persistence/repository/mysql/MySQLRepositoryExceptionHandler.java
+++ src/main/java/com/vluepixel/vetmanager/api/shared/adapter/out/persistence/repository/mysql/MySQLRepositoryExceptionHandler.java
@@ -12,6 +12,7 @@ import org.hibernate.query.sqm.PathElementException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.TransactionSystemException;
 
@@ -26,6 +27,7 @@ import com.vluepixel.vetmanager.api.shared.domain.repository.RepositoryErrorType
 import com.vluepixel.vetmanager.api.shared.domain.repository.RepositoryExceptionHandler;
 import com.vluepixel.vetmanager.api.shared.domain.validation.ValidationError;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.NonUniqueResultException;
 import lombok.RequiredArgsConstructor;
@@ -53,6 +55,10 @@ public final class MySQLRepositoryExceptionHandler implements RepositoryExceptio
         }
 
         else if (e instanceof TransactionSystemException) {
+            handle(e.getCause(), entityClass);
+        }
+
+        else if (e instanceof JpaObjectRetrievalFailureException) {
             handle(e.getCause(), entityClass);
         }
 
@@ -89,6 +95,10 @@ public final class MySQLRepositoryExceptionHandler implements RepositoryExceptio
         }
 
         else if (e instanceof NonUniqueResultException childException) {
+            handle(childException, entityClass);
+        }
+
+        else if (e instanceof EntityNotFoundException childException) {
             handle(childException, entityClass);
         }
 
@@ -176,5 +186,12 @@ public final class MySQLRepositoryExceptionHandler implements RepositoryExceptio
 
     private void handle(StaleObjectStateException e, Class<?> entityClass) {
         throw new InternalServerErrorException(e);
+    }
+
+    private void handle(EntityNotFoundException e, Class<?> entityClass) {
+        String[] splittedMessage = e.getMessage().split("\\.");
+        String entityName = splittedMessage[splittedMessage.length - 1].split(" ")[0];
+
+        throw new NotFoundException(entityName);
     }
 }
