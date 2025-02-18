5de77bf1edffd4e8559655fd4e01d355b9b9ee74
Martín Alexander Ramos Yampufé <114754842+martindotpy@users.noreply.github.com>
2025-02-18 15:56:37 -0500

    Revert "refactor: Remove SpanishName annotation and improve exception messages"
    This reverts commit 04fa8e4603e5c041a6a18260ae54f7af99b284e7.

--- src/main/java/com/vluepixel/vetmanager/api/shared/domain/exception/ErrorException.java
+++ src/main/java/com/vluepixel/vetmanager/api/shared/domain/exception/ErrorException.java
@@ -1,8 +1,5 @@
 package com.vluepixel.vetmanager.api.shared.domain.exception;
 
-/**
- * Error exception.
- */
 public abstract class ErrorException extends RuntimeException {
     public abstract int getStatus();
 }
