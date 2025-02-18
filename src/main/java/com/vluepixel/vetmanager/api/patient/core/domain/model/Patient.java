c930fe7c4ee079b7128b14e99703573e9793e7d1
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-18 16:00:56 -0500

    chore: Remove unnused import

--- src/main/java/com/vluepixel/vetmanager/api/patient/core/domain/model/Patient.java
+++ src/main/java/com/vluepixel/vetmanager/api/patient/core/domain/model/Patient.java
@@ -24,7 +24,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
