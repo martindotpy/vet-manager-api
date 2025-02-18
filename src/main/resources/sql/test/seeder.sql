60805b60691ed498cf7cfd767b752a790dc52e3e
Martín Alexander Ramos Yampufé <114754842+martindotpy@users.noreply.github.com>
2025-02-18 15:56:37 -0500

    Revert "style: Format sql script"
    This reverts commit a4ba5937a4d8fdf666d699ea72193bc15c2b7de5.

--- src/main/resources/sql/test/seeder.sql
+++ src/main/resources/sql/test/seeder.sql
@@ -62,31 +62,78 @@ VALUES
     (1, 'firstclient@firstclient.com'),
     (2, 'secondclient@secondclient.com');
 
-INSERT INTO
-    `client_emails` (client_id, emails)
-VALUES
-    (1, 'firstclient@firstclient.com'),
-    (2, 'secondclient@secondclient.com');
+INSERT INTO 
+    `client_emails` (
+        client_id,
+        emails
+    )  
+VALUES  
+    (
+        1,
+        'firstclient@firstclient.com'
+    ),
+    (
+        2,
+        'secondclient@secondclient.com'
+    );
+
+INSERT INTO 
+    `client_phones` (
+        client_id,
+        phones
+    )  
+VALUES  
+    (
+        1,
+        '999999999'
+    ),
+    (
+        2,
+        '922222222'
+    );
 
 INSERT INTO
-    `client_phones` (client_id, phones)
+    `species` (
+        id,
+        `name`
+    )
 VALUES
-    (1, '999999999'),
-    (2, '922222222');
+    (
+        1,
+        'Perro'
+    ),
+    (
+        2,
+        'Gato'
+    );
 
 INSERT INTO
-    `species` (id, `name`)
+    `race` (
+        id,
+        species_id,
+        `name`
+    )
 VALUES
-    (1, 'Perro'),
-    (2, 'Gato');
-
-INSERT INTO
-    `race` (id, species_id, `name`)
-VALUES
-    (1, 1, 'Poodle'),
-    (2, 1, 'Chihuahua'),
-    (3, 2, 'Siamés'),
-    (4, 2, 'Persa');
+    (
+        1,
+        1,
+        'Poodle'
+    ),
+    (
+        2,
+        1,
+        'Chihuahua'
+    ),
+    (
+        3,
+        2,
+        'Siamés'
+    ),
+    (
+        4,
+        2,
+        'Persa'
+    );
 
 INSERT INTO
     `patient` (
