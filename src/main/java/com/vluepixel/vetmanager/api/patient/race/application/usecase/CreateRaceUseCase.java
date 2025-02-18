4948a5f3525a84afa27b6276db5b1708c27c00a5
Martín Alexander Ramos Yampufé <114754842+martindotpy@users.noreply.github.com>
2025-02-18 15:56:37 -0500

    Revert "feat: Load species info"
    This reverts commit 2e9f14492095039b5ea179e60440c0106d028826.

--- src/main/java/com/vluepixel/vetmanager/api/patient/race/application/usecase/CreateRaceUseCase.java
+++ src/main/java/com/vluepixel/vetmanager/api/patient/race/application/usecase/CreateRaceUseCase.java
@@ -5,11 +5,8 @@ import org.slf4j.MDC;
 import com.vluepixel.vetmanager.api.patient.race.application.dto.RaceDto;
 import com.vluepixel.vetmanager.api.patient.race.application.mapper.RaceMapper;
 import com.vluepixel.vetmanager.api.patient.race.application.port.in.CreateRacePort;
-import com.vluepixel.vetmanager.api.patient.race.domain.model.Race;
 import com.vluepixel.vetmanager.api.patient.race.domain.repository.RaceRepository;
 import com.vluepixel.vetmanager.api.patient.race.domain.request.CreateRaceRequest;
-import com.vluepixel.vetmanager.api.patient.species.domain.model.Species;
-import com.vluepixel.vetmanager.api.patient.species.domain.repository.SpeciesRepository;
 import com.vluepixel.vetmanager.api.shared.application.annotation.UseCase;
 
 import jakarta.transaction.Transactional;
@@ -23,8 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 @UseCase
 @RequiredArgsConstructor
 public class CreateRaceUseCase implements CreateRacePort {
-    private final SpeciesRepository speciesRepository;
-
     private final RaceRepository raceRepository;
     private final RaceMapper raceMapper;
 
@@ -34,12 +29,8 @@ public class CreateRaceUseCase implements CreateRacePort {
         MDC.put("operationId", "Race name " + request.getName());
         log.info("Creating race");
 
-        Race newRace = raceMapper.fromRequest(request).build();
+        var newRace = raceMapper.fromRequest(request).build();
         newRace = raceRepository.save(newRace);
-
-        // Get the species name
-        Species species = speciesRepository.findById(newRace.getSpecies().getId()).get();
-        newRace = raceMapper.toBuilder(newRace).species(species).build();
 
         log.info("Race created");
 
