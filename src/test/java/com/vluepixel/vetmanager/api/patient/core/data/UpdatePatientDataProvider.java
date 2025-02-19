f9d2419a32b71d105df8868d5c1235f3906e391e
zincognity <67128655+zincognity@users.noreply.github.com>
2025-02-19 15:25:10 -0500

    test(patient): PUT method has been implemented
    Refs: #61

---
+++ src/test/java/com/vluepixel/vetmanager/api/patient/core/data/UpdatePatientDataProvider.java
@@ -0,0 +1,25 @@
+package com.vluepixel.vetmanager.api.patient.core.data;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import com.vluepixel.vetmanager.api.patient.core.domain.enums.PatientGender;
+import com.vluepixel.vetmanager.api.patient.core.domain.request.UpdatePatientRequest;
+
+/**
+ * Update patient data provider.
+ */
+public class UpdatePatientDataProvider {
+    public static final UpdatePatientRequest INVALID_UPDATE_CLIENT_REQUEST = UpdatePatientRequest
+            .builder()
+            .id(10L)
+            .name("New name")
+            .birthDate(LocalDate.now())
+            .gender(PatientGender.FEMALE)
+            .characteristics("Le creció más el pelo")
+            .deceased(true)
+            .raceId(2)
+            .ownerId(2L)
+            .build();
+
+}
