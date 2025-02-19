edf868bd925a8f38c32c5b8a6af6ab8344c571d6
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-19 05:46:57 -0500

    fix(appointment): Add validations

--- src/main/java/com/vluepixel/vetmanager/api/appointment/core/domain/request/CreateAppointmentRequest.java
+++ src/main/java/com/vluepixel/vetmanager/api/appointment/core/domain/request/CreateAppointmentRequest.java
@@ -6,8 +6,10 @@ import java.util.List;
 import com.vluepixel.vetmanager.api.appointment.details.domain.request.CreateAppointmentDetailsRequest;
 import com.vluepixel.vetmanager.api.shared.domain.request.Request;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,9 +25,12 @@ import lombok.NoArgsConstructor;
 public final class CreateAppointmentRequest implements Request {
     @NotNull(message = "La fecha de inicio es requerida")
     private LocalDateTime startAt;
+    @NotNull(message = "La descripción es requerida")
     private String description;
 
     @NotEmpty(message = "La lista de detalles es requerido")
-    private List<@NotNull(message = "Ningún detalle puede ser nulo") CreateAppointmentDetailsRequest> details;
+    private List<@Valid @NotNull(message = "Ningún detalle puede ser nulo") CreateAppointmentDetailsRequest> details;
+    @NotNull(message = "El id del paciente es requerido")
+    @Positive(message = "El id del paciente debe ser mayor a 0")
     private Long patientId;
 }
