54f1ef3053675a12a58171ccda43b225e897a15d
zincognity <67128655+zincognity@users.noreply.github.com>
2025-02-20 03:56:25 -0500

    test(appointment/type): DELETE method has been implemented successfully
    Closes: #40

---
+++ src/test/java/com/vluepixel/vetmanager/api/appointment/core/type/integration/DeleteAppointmentTypeIntegrationTest.java
@@ -0,0 +1,160 @@
+package com.vluepixel.vetmanager.api.appointment.core.type.integration;
+
+import static com.vluepixel.vetmanager.api.appointment.core.type.data.UpdateAppointmentTypeDataProvider.INVALID_UPDATE_APPOINTMENT_TYPE_REQUEST;
+import static com.vluepixel.vetmanager.api.appointment.core.type.data.UpdateAppointmentTypeDataProvider.VALID_UPDATE_APPOINTMENT_TYPE_REQUEST;
+import static com.vluepixel.vetmanager.api.auth.core.data.AuthDataProvider.BEARER_ADMIN_JWT;
+import static com.vluepixel.vetmanager.api.auth.core.data.AuthDataProvider.BEARER_USER_JWT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.function.Function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.DirtiesContext;
+
+import com.vluepixel.vetmanager.api.base.BaseIntegrationTest;
+
+/**
+ * Integration tests for the delete appointment type functionality.
+ */
+public class DeleteAppointmentTypeIntegrationTest extends BaseIntegrationTest {
+    private static final String MESSAGE_OK = "Tipo de cita eliminada";
+    private static final Function<String, String> MESSAGE_NOT_FOUND = parameter -> String
+            .format("Tipo de cita con id %s no encontrado(a)", parameter);
+    // -----------------------------------------------------------------------------------------------------------------
+    // Without authentication:
+    // -----------------------------------------------------------------------------------------------------------------
+
+    @Test
+    void noUser_DeleteAppointmentTypeWithValidParams_Forbidden() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", VALID_UPDATE_APPOINTMENT_TYPE_REQUEST.getId()))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.message").value(MESSAGE_FORBIDDEN));
+    }
+
+    @Test
+    void noUser_DeleteAppointmentTypeWithInvalidParams_ID_NotFound_Forbidden() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", INVALID_UPDATE_APPOINTMENT_TYPE_REQUEST.getId()))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.message").value(MESSAGE_FORBIDDEN));
+    }
+
+    @Test
+    void noUser_DeleteAppointmentTypeWithInvalidParams_ID_Negative_Forbidden() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", -10))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.message").value(MESSAGE_FORBIDDEN));
+    }
+
+    @Test
+    void noUser_DeleteAppointmentTypeWithInvalidParams_ID_NotNumber_Forbidden() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", "abcde"))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.message").value(MESSAGE_FORBIDDEN));
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // With authentication:
+    // -----------------------------------------------------------------------------------------------------------------
+
+    // Role: USER
+
+    @Test
+    @DirtiesContext
+    void user_DeleteAppointmentTypeWithValidParams_Ok() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", VALID_UPDATE_APPOINTMENT_TYPE_REQUEST.getId())
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(MESSAGE_OK));
+    }
+
+    @Test
+    void user_DeleteAppointmentTypeWithInvalidParams_ID_NotFound_NotFound() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", INVALID_UPDATE_APPOINTMENT_TYPE_REQUEST.getId())
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isNotFound())
+                .andExpect(
+                        jsonPath("$.message")
+                                .value(MESSAGE_NOT_FOUND
+                                        .apply(INVALID_UPDATE_APPOINTMENT_TYPE_REQUEST.getId().toString())));
+    }
+
+    @Test
+    void user_DeleteAppointmentTypeWithInvalidParams_ID_Negative_UnprocessableEntity() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", -10)
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpectAll(
+                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
+                        jsonPath("$.details").isArray(),
+                        jsonPath("$.details.length()").value(1),
+                        jsonPath("$.details[0].field").value("query.id"),
+                        jsonPath("$.details[0].messages.length()").value(1),
+                        jsonPath("$.details[0].messages").value("El id debe ser mayor a 0"));
+    }
+
+    @Test
+    void user_DeleteAppointmentTypeWithInvalidParams_ID_NotNumber_UnprocessableEntity() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", "abcde")
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpectAll(
+                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
+                        jsonPath("$.details").isArray(),
+                        jsonPath("$.details.length()").value(1),
+                        jsonPath("$.details[0].field").value("path.id"),
+                        jsonPath("$.details[0].messages.length()").value(1),
+                        jsonPath("$.details[0].messages").value("Valor numérico inválido"));
+    }
+
+    // Role: ADMIN
+    @Test
+    @DirtiesContext
+    void admin_DeleteAppointmentTypeWithValidParams_Ok() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", VALID_UPDATE_APPOINTMENT_TYPE_REQUEST.getId())
+                .header("Authorization", BEARER_ADMIN_JWT))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(MESSAGE_OK));
+    }
+
+    @Test
+    void admin_DeleteAppointmentTypeWithInvalidParams_ID_NotFound_NotFound() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", INVALID_UPDATE_APPOINTMENT_TYPE_REQUEST.getId())
+                .header("Authorization", BEARER_ADMIN_JWT))
+                .andExpect(status().isNotFound())
+                .andExpect(
+                        jsonPath("$.message")
+                                .value(MESSAGE_NOT_FOUND
+                                        .apply(INVALID_UPDATE_APPOINTMENT_TYPE_REQUEST.getId().toString())));
+    }
+
+    @Test
+    void admin_DeleteAppointmentTypeWithInvalidParams_ID_Negative_UnprocessableEntity() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", -10)
+                .header("Authorization", BEARER_ADMIN_JWT))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpectAll(
+                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
+                        jsonPath("$.details").isArray(),
+                        jsonPath("$.details.length()").value(1),
+                        jsonPath("$.details[0].field").value("query.id"),
+                        jsonPath("$.details[0].messages.length()").value(1),
+                        jsonPath("$.details[0].messages").value("El id debe ser mayor a 0"));
+    }
+
+    @Test
+    void admin_DeleteAppointmentTypeWithInvalidParams_ID_NotNumber_UnprocessableEntity() throws Exception {
+        mockMvc.perform(delete("/appointment/type/{id}", "abcde")
+                .header("Authorization", BEARER_ADMIN_JWT))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpectAll(
+                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
+                        jsonPath("$.details").isArray(),
+                        jsonPath("$.details.length()").value(1),
+                        jsonPath("$.details[0].field").value("path.id"),
+                        jsonPath("$.details[0].messages.length()").value(1),
+                        jsonPath("$.details[0].messages").value("Valor numérico inválido"));
+    }
+
+}
