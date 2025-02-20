6b4f65dc06affa0fd6e65ac64453a4061bc0514a
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-20 02:07:28 -0500

    feat(sale): enhance CreateSale and UpdateSale use cases with product quantity management

--- src/main/java/com/vluepixel/vetmanager/api/bill/sale/application/usecase/CreateSaleUseCase.java
+++ src/main/java/com/vluepixel/vetmanager/api/bill/sale/application/usecase/CreateSaleUseCase.java
@@ -5,10 +5,17 @@ import org.slf4j.MDC;
 import com.vluepixel.vetmanager.api.bill.sale.application.dto.SaleDto;
 import com.vluepixel.vetmanager.api.bill.sale.application.mapper.SaleMapper;
 import com.vluepixel.vetmanager.api.bill.sale.application.port.in.CreateSalePort;
+import com.vluepixel.vetmanager.api.bill.sale.domain.model.AppointmentSale;
+import com.vluepixel.vetmanager.api.bill.sale.domain.model.ProductSale;
 import com.vluepixel.vetmanager.api.bill.sale.domain.model.Sale;
+import com.vluepixel.vetmanager.api.bill.sale.domain.model.TreatmentSale;
 import com.vluepixel.vetmanager.api.bill.sale.domain.repository.SaleRepository;
 import com.vluepixel.vetmanager.api.bill.sale.domain.request.CreateSaleRequest;
+import com.vluepixel.vetmanager.api.product.core.domain.model.Product;
+import com.vluepixel.vetmanager.api.product.core.domain.repository.ProductRepository;
 import com.vluepixel.vetmanager.api.shared.application.annotation.UseCase;
+import com.vluepixel.vetmanager.api.shared.application.port.out.TransactionalPort;
+import com.vluepixel.vetmanager.api.shared.domain.exception.NotFoundException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +27,10 @@ import lombok.extern.slf4j.Slf4j;
 @UseCase
 @RequiredArgsConstructor
 public class CreateSaleUseCase implements CreateSalePort {
+    private final TransactionalPort transactionalPort;
+
+    private final ProductRepository productRepository;
+
     private final SaleRepository saleRepository;
     private final SaleMapper saleMapper;
 
@@ -29,10 +40,35 @@ public class CreateSaleUseCase implements CreateSalePort {
         log.info("Creating sale");
 
         Sale newSale = saleMapper.fromRequest(request);
-        newSale = saleRepository.save(newSale);
+        Sale newSaleAux = transactionalPort.run((aux) -> {
+            switch (newSale) {
+                case ProductSale productSale -> {
+                    aux.setEntityClass(Product.class);
+
+                    Integer quantity = productSale.getQuantity();
+
+                    Long productId = productSale.getProduct().getId();
+                    Product product = productRepository.findById(productId)
+                            .orElseThrow(() -> new NotFoundException(Product.class, productId));
+                    product.reduceQuantity(quantity);
+
+                    productRepository.save(product);
+
+                    aux.setEntityClass(productSale.getClass());
+                }
+                case AppointmentSale appointmentSale -> {
+                    aux.setEntityClass(appointmentSale.getClass());
+                }
+                case TreatmentSale treatmentSale -> {
+                    aux.setEntityClass(treatmentSale.getClass());
+                }
+            }
+
+            return saleRepository.save(newSale);
+        });
 
         log.info("Sale created");
 
-        return saleMapper.toDto(newSale);
+        return saleMapper.toDto(newSaleAux);
     }
 }
--- src/main/java/com/vluepixel/vetmanager/api/bill/sale/application/usecase/UpdateSaleUseCase.java
+++ src/main/java/com/vluepixel/vetmanager/api/bill/sale/application/usecase/UpdateSaleUseCase.java
@@ -5,10 +5,18 @@ import org.slf4j.MDC;
 import com.vluepixel.vetmanager.api.bill.sale.application.dto.SaleDto;
 import com.vluepixel.vetmanager.api.bill.sale.application.mapper.SaleMapper;
 import com.vluepixel.vetmanager.api.bill.sale.application.port.in.UpdateSalePort;
+import com.vluepixel.vetmanager.api.bill.sale.domain.model.AppointmentSale;
+import com.vluepixel.vetmanager.api.bill.sale.domain.model.ProductSale;
 import com.vluepixel.vetmanager.api.bill.sale.domain.model.Sale;
+import com.vluepixel.vetmanager.api.bill.sale.domain.model.TreatmentSale;
 import com.vluepixel.vetmanager.api.bill.sale.domain.repository.SaleRepository;
 import com.vluepixel.vetmanager.api.bill.sale.domain.request.UpdateSaleRequest;
+import com.vluepixel.vetmanager.api.product.core.domain.model.Product;
+import com.vluepixel.vetmanager.api.product.core.domain.repository.ProductRepository;
 import com.vluepixel.vetmanager.api.shared.application.annotation.UseCase;
+import com.vluepixel.vetmanager.api.shared.application.port.out.TransactionalPort;
+import com.vluepixel.vetmanager.api.shared.domain.exception.NotFoundException;
+import com.vluepixel.vetmanager.api.shared.domain.exception.RegisterNotInstanceOfSubclassException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +28,10 @@ import lombok.extern.slf4j.Slf4j;
 @UseCase
 @RequiredArgsConstructor
 public class UpdateSaleUseCase implements UpdateSalePort {
+    private final TransactionalPort transactionalPort;
+
+    private final ProductRepository productRepository;
+
     private final SaleRepository saleRepository;
     private final SaleMapper saleMapper;
 
@@ -29,10 +41,46 @@ public class UpdateSaleUseCase implements UpdateSalePort {
         log.info("Updating sale");
 
         Sale updatedSale = saleMapper.fromRequest(request);
-        updatedSale = saleRepository.save(updatedSale);
+        Sale updatedSaleAux = transactionalPort.run((aux) -> {
+            switch (updatedSale) {
+                case ProductSale productSale -> {
+                    aux.setEntityClass(Sale.class);
+
+                    Sale sale = saleRepository.findById(productSale.getId())
+                            .orElseThrow(() -> new NotFoundException(Sale.class, productSale.getId()));
+
+                    if (!(sale instanceof ProductSale)) {
+                        throw new RegisterNotInstanceOfSubclassException(ProductSale.class);
+                    }
+
+                    ProductSale preUpdateProductSale = (ProductSale) sale;
+
+                    aux.setEntityClass(Product.class);
+
+                    Integer quantity = productSale.getQuantity() - preUpdateProductSale.getQuantity();
+
+                    Long productId = productSale.getProduct().getId();
+                    Product product = productRepository.findById(productId)
+                            .orElseThrow(() -> new NotFoundException(Product.class, productId));
+                    product.reduceQuantity(quantity);
+
+                    productRepository.save(product);
+
+                    aux.setEntityClass(productSale.getClass());
+                }
+                case AppointmentSale appointmentSale -> {
+                    aux.setEntityClass(appointmentSale.getClass());
+                }
+                case TreatmentSale treatmentSale -> {
+                    aux.setEntityClass(treatmentSale.getClass());
+                }
+            }
+
+            return saleRepository.save(updatedSale);
+        });
 
         log.info("Sale updated");
 
-        return saleMapper.toDto(updatedSale);
+        return saleMapper.toDto(updatedSaleAux);
     }
 }
