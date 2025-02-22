b1c46ca575e563d4017e40bc77e836961d633073
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-22 01:15:30 -0500

    feat(bill): Add validation when total paid is greather then total

--- src/main/java/com/vluepixel/vetmanager/api/bill/core/application/usecase/UpdateBillUseCase.java
+++ src/main/java/com/vluepixel/vetmanager/api/bill/core/application/usecase/UpdateBillUseCase.java
@@ -1,5 +1,7 @@
 package com.vluepixel.vetmanager.api.bill.core.application.usecase;
 
+import java.util.List;
+
 import org.slf4j.MDC;
 
 import com.vluepixel.vetmanager.api.bill.core.application.dto.BillDto;
@@ -11,6 +13,8 @@ import com.vluepixel.vetmanager.api.bill.core.domain.request.UpdateBillRequest;
 import com.vluepixel.vetmanager.api.client.core.domain.model.Client;
 import com.vluepixel.vetmanager.api.shared.application.annotation.UseCase;
 import com.vluepixel.vetmanager.api.shared.domain.exception.NotFoundException;
+import com.vluepixel.vetmanager.api.shared.domain.exception.ValidationException;
+import com.vluepixel.vetmanager.api.shared.domain.validation.ValidationError;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -32,6 +36,11 @@ public class UpdateBillUseCase implements UpdateBillPort {
 
         Bill preUpdateBill = billRepository.findById(request.getId())
                 .orElseThrow(() -> new NotFoundException(Bill.class, request.getId()));
+
+        if (preUpdateBill.getTotal().compareTo(request.getTotalPaid()) < 0) {
+            throw new ValidationException(List.of(
+                    new ValidationError("total_paid", "El total pagado no puede ser mayor al total de la cuenta")));
+        }
 
         Bill updatedBill = billMapper.toBuilder(preUpdateBill)
                 .discount(request.getDiscount())
