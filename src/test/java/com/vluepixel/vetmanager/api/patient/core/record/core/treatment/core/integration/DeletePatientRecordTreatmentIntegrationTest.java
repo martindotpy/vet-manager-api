53ad6eb40a2cca7ec532b51d3394fae9ce527a6f
zincognity <67128655+zincognity@users.noreply.github.com>
2025-02-27 22:31:20 -0500

    test(patient/record/treatment): DELETE method has been updated
    There are invalidations to fixed
    
    Refs: #139

--- src/test/java/com/vluepixel/vetmanager/api/patient/core/record/core/treatment/core/integration/DeletePatientRecordTreatmentIntegrationTest.java
+++ src/test/java/com/vluepixel/vetmanager/api/patient/core/record/core/treatment/core/integration/DeletePatientRecordTreatmentIntegrationTest.java
@@ -5,6 +5,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.function.Function;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -14,7 +16,11 @@ import com.vluepixel.vetmanager.api.base.BaseIntegrationTest;
  * Integration tests for the delete patient record treatment use case.
  */
 public class DeletePatientRecordTreatmentIntegrationTest extends BaseIntegrationTest {
-    private static final String MESSAGE_OK = "";
+    private static final String MESSAGE_OK = "Tratamiento eliminado exitosamente";
+    private static final Function<String, String> MESSAGE_PATIENT_NOT_FOUND = parameter -> String
+            .format("Paciente con id %s no encontrado(a)", parameter);
+    private static Function<String, String> MESSAGE_TREATMENT_NOT_FOUND = parameter -> String
+            .format("Tratamiento con id %s no encontrado(a)", parameter);
     // -----------------------------------------------------------------------------------------------------------------
     // Without authentication:
     // -----------------------------------------------------------------------------------------------------------------
@@ -35,4 +41,118 @@ public class DeletePatientRecordTreatmentIntegrationTest extends BaseIntegration
                 .andExpect(jsonPath("$.message").value(MESSAGE_OK));
     }
 
+    // Patient ID
+    @Test
+    void admin_DeletePatientRecordTreatmentWithInvalidParams_PatientID_NotFound_NotFound() throws Exception {
+        mockMvc.perform(delete("/patient/{patient_id}/record/{record_id}/treatment/{id}", 10, 1, 1)
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(MESSAGE_PATIENT_NOT_FOUND.apply("10")));
+    }
+
+    @Test
+    void admin_DeletePatientRecordTreatmentWithInvalidParams_PatientID_Negative_UnprocessableEntity() throws Exception {
+        mockMvc.perform(delete("/patient/{patient_id}/record/{record_id}/treatment/{id}", -10, 1, 1)
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpectAll(
+                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
+                        jsonPath("$.details").isArray(),
+                        jsonPath("$.details.length()").value(1),
+                        jsonPath("$.details[0].field").value("path.patient_id"),
+                        jsonPath("$.details[0].messages.length()").value(1),
+                        jsonPath("$.details[0].messages").value("El id del paciente debe ser mayor a 0"));
+    }
+
+    @Test
+    void admin_DeletePatientRecordTreatmentWithInvalidParams_PatientID_NotNumber_UnprocessableEntity()
+            throws Exception {
+        mockMvc.perform(delete("/patient/{patient_id}/record/{record_id}/treatment/{id}", "abcde", 1, 1)
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpectAll(
+                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
+                        jsonPath("$.details").isArray(),
+                        jsonPath("$.details.length()").value(1),
+                        jsonPath("$.details[0].field").value("path.patient_id"),
+                        jsonPath("$.details[0].messages.length()").value(1),
+                        jsonPath("$.details[0].messages").value("Valor numérico inválido"));
+    }
+
+    // Record ID
+    @Test // TODO: Should be returns Record Not Found but return patient is Not Found
+    void admin_DeletePatientRecordTreatmentWithInvalidParams_RecordID_NotFound_NotFound() throws Exception {
+        mockMvc.perform(delete("/patient/{patient_id}/record/{record_id}/treatment/{id}", 1, 10, 1)
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(MESSAGE_PATIENT_NOT_FOUND.apply("1")));
+    }
+
+    @Test
+    void admin_DeletePatientRecordTreatmentWithInvalidParams_RecordID_Negative_UnprocessableEntity() throws Exception {
+        mockMvc.perform(delete("/patient/{patient_id}/record/{record_id}/treatment/{id}", 1, -10, 1)
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpectAll(
+                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
+                        jsonPath("$.details").isArray(),
+                        jsonPath("$.details.length()").value(1),
+                        jsonPath("$.details[0].field").value("path.record_id"),
+                        jsonPath("$.details[0].messages.length()").value(1),
+                        jsonPath("$.details[0].messages").value("El id del historial médico debe ser mayor a 0"));
+    }
+
+    @Test
+    void admin_DeletePatientRecordTreatmentWithInvalidParams_RecordID_NotNumber_UnprocessableEntity()
+            throws Exception {
+        mockMvc.perform(delete("/patient/{patient_id}/record/{record_id}/treatment/{id}", 1, "abcde", 1)
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpectAll(
+                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
+                        jsonPath("$.details").isArray(),
+                        jsonPath("$.details.length()").value(1),
+                        jsonPath("$.details[0].field").value("path.record_id"),
+                        jsonPath("$.details[0].messages.length()").value(1),
+                        jsonPath("$.details[0].messages").value("Valor numérico inválido"));
+    }
+
+    // Treatment ID
+    @Test
+    void admin_DeletePatientRecordTreatmentWithInvalidParams_TreatmentID_NotFound_NotFound() throws Exception {
+        mockMvc.perform(delete("/patient/{patient_id}/record/{record_id}/treatment/{id}", 1, 1, 10)
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(MESSAGE_TREATMENT_NOT_FOUND.apply("10")));
+    }
+
+    @Test
+    void admin_DeletePatientRecordTreatmentWithInvalidParams_TreatmentID_Negative_UnprocessableEntity()
+            throws Exception {
+        mockMvc.perform(delete("/patient/{patient_id}/record/{record_id}/treatment/{id}", 1, 1, -10)
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpectAll(
+                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
+                        jsonPath("$.details").isArray(),
+                        jsonPath("$.details.length()").value(1),
+                        jsonPath("$.details[0].field").value("query.id"),
+                        jsonPath("$.details[0].messages.length()").value(1),
+                        jsonPath("$.details[0].messages").value("El id debe ser mayor a 0"));
+    }
+
+    @Test
+    void admin_DeletePatientRecordTreatmentWithInvalidParams_TreatmentID_NotNumber_UnprocessableEntity()
+            throws Exception {
+        mockMvc.perform(delete("/patient/{patient_id}/record/{record_id}/treatment/{id}", 1, 1, "abcde")
+                .header("Authorization", BEARER_USER_JWT))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpectAll(
+                        jsonPath("$.message").value(MESSAGE_UNPROCESSABLE_ENTITY),
+                        jsonPath("$.details").isArray(),
+                        jsonPath("$.details.length()").value(1),
+                        jsonPath("$.details[0].field").value("path.id"),
+                        jsonPath("$.details[0].messages.length()").value(1),
+                        jsonPath("$.details[0].messages").value("Valor numérico inválido"));
+    }
 }
